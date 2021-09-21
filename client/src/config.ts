56914b1d68303d934e665cd10fbcf7345e26e461
Dang Khoa Nguyen <khoanguyen91@outlook.com>
2021-09-21 13:14:51 +0700

    aaa

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  //baseURL : "https://practice-sheyshop.herokuapp.com/"
-  baseURL : "http://localhost:8000"
+  baseURL : "https://practice-sheyshop.herokuapp.com/"
+  //baseURL : "http://localhost:8000"
 })
